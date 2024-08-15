0a36dea5c295dd2acd375f6bbc35e9f8368f55a2
Ayush Behera <himanishayush2003@gmail.com>
2024-08-15 18:25:53 +0530

    git commit -m "added new files"

---
+++ CPP/string.cpp
@@ -0,0 +1,11 @@
+// reverse a string
+#include<iostream>
+using namespace std;
+int main(){
+    string str;
+    // char n[50];
+    cout<<"enter the string: "<<endl;
+    //gets(n);
+    getline(cin,str);
+    cout<<"entered strings are: "<<str<<endl;
+}
