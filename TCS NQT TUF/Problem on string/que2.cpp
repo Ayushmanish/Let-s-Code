385f7fbcda7dad544f064afdcc253836d65d10c0
Ayush Behera <himanishayush2003@gmail.com>
2024-08-26 00:08:02 +0530

    git commit -m "modified two files"

--- TCS NQT TUF/Problem on string/que2.cpp
+++ TCS NQT TUF/Problem on string/que2.cpp
@@ -8,7 +8,31 @@
 // }
 
 
-// Remove all vowels from the string
+// // Remove all vowels from the string
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main()
+// {
+//     string str;
+//     getline(cin,str);
+//     int i,size;
+//     vector<char>vec;
+//     for(i=0;i<str.length();i++){
+//         if(isalpha(str[i]) || str[i]==' '){
+//             if(tolower(str[i])=='a' || tolower(str[i])=='e' || tolower(str[i])=='i' || tolower(str[i])=='o' || tolower(str[i])=='u'){
+//                 continue;
+//             }
+//             else
+//             vec.push_back(str[i]);
+//         }
+//     }
+//     for(i=0;i<vec.size();i++)
+//     cout<<vec[i];
+//     cout<<"\n"<<str.substr(3,8);
+// }
+
+//2nd approach for the above problem without taking any second array
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -17,17 +41,10 @@ int main()
     string str;
     getline(cin,str);
     int i,size;
-    vector<char>vec;
     for(i=0;i<str.length();i++){
-        if(isalpha(str[i]) || str[i]==' '){
-            if(tolower(str[i])=='a' || tolower(str[i])=='e' || tolower(str[i])=='i' || tolower(str[i])=='o' || tolower(str[i])=='u'){
-                continue;
-            }
-            else
-            vec.push_back(str[i]);
+        if(tolower(str[i])=='a' || tolower(str[i])=='e' || tolower(str[i])=='i' || tolower(str[i])=='o' || tolower(str[i])=='u'){
+            str=str.substr(0,i)+str.substr(i+1);
         }
     }
-    for(i=0;i<vec.size();i++)
-    cout<<vec[i];
-    cout<<"\n"<<str.substr(3,8);
+    cout<<"\nthe updated string is: "<<str;
 }
